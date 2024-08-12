bd8656a29ca14c7e8d8fb4da6ad9838c0de31b0b
ridhozhr10 <azharridho42@gmail.com>
2024-08-12 16:08:57 +0700

    feat: instagram crawl

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/url"
 	"os"
+	"regexp"
+	"strconv"
 	"strings"
 	"time"
 
@@ -31,7 +34,8 @@ func main() {
 	}
 
 	// FacebookPOC(context)
-	TwitterPOC(context)
+	// TwitterPOC(context)
+	InstagramPOC(context)
 }
 
 func TwitterPOC(context playwright.BrowserContext) {
@@ -253,7 +257,6 @@ func FacebookPOC(context playwright.BrowserContext) {
 	}
 	page.GetByRole("link", playwright.PageGetByRoleOptions{Name: "Timeline"}).Click()
 	targetPage := 5
-
 	// Convert results to CSV
 
 	// Write CSV data to a file
@@ -270,6 +273,7 @@ func FacebookPOC(context playwright.BrowserContext) {
 	}
 
 	results := []map[string]interface{}{}
+
 	for i := 0; i < targetPage; i++ {
 		page.WaitForSelector("div .feed", playwright.PageWaitForSelectorOptions{Timeout: playwright.Float(60000)})
 		articlesLocator := page.Locator(`article[data-ft='{"tn":"-R"}']`)
@@ -318,5 +322,193 @@ func FacebookPOC(context playwright.BrowserContext) {
 		}
 		page.GetByRole("link", playwright.PageGetByRoleOptions{Name: "See more stories"}).Click()
 	}
+}
 
+type Result struct {
+	Link         string
+	TotalComment int
+	TotalLike    int
+	Username     string
+	Date         time.Time
+	Post         string
+}
+
+func (res Result) ToCSV() string {
+	return fmt.Sprintf(
+		"%s,%s,%s,%s,%s,%s,%s\n",
+		url.QueryEscape(res.Username),
+		url.QueryEscape(res.Post),
+		url.QueryEscape(res.Date.String()),
+		url.QueryEscape(res.Link),
+		strconv.Itoa(res.TotalLike),
+		strconv.Itoa(res.TotalComment),
+		"false",
+	)
+}
+
+func InstagramPOC(ctx playwright.BrowserContext) {
+	dat, err := os.ReadFile("./cookies-ig-icjonoss.txt")
+	if err != nil {
+		log.Fatalf("failed to read cookies file: %v", err)
+		return
+	}
+
+	cookiesRaw := string(dat)
+	re := regexp.MustCompile("\t|\n")
+	cookieSplitRaw := re.Split(cookiesRaw, -1)
+	// parsing empty
+	cookieSplit := []string{}
+	for _, v := range cookieSplitRaw {
+		if len(v) > 0 {
+			cookieSplit = append(cookieSplit, v)
+		}
+	}
+
+	cookies := []playwright.OptionalCookie{}
+	for i := 0; i < len(cookieSplit)/7; i++ {
+		idx := i * 7
+		expire, _ := strconv.ParseFloat(cookieSplit[idx+4], 64)
+		nc := playwright.OptionalCookie{
+			Name:     cookieSplit[idx+5],
+			Value:    cookieSplit[idx+6],
+			Domain:   playwright.String(cookieSplit[idx+0]),
+			Path:     playwright.String(cookieSplit[idx+2]),
+			Expires:  playwright.Float(expire),
+			HttpOnly: playwright.Bool(true),
+			Secure:   playwright.Bool(true),
+		}
+		cookies = append(cookies, nc)
+	}
+
+	if err := ctx.AddCookies(cookies); err != nil {
+		log.Fatalf("failed to set cookie: %v", err)
+	}
+
+	page, err := ctx.NewPage()
+	targetURI := "https://www.instagram.com/kementerian.atrbpn/"
+	if _, err := page.Goto(targetURI); err != nil {
+		log.Fatalf("failed to open page %v : %v", targetURI, err)
+	}
+
+	rows, err := page.Locator("main > div > div:nth-child(3) > div > div").All()
+	if err != nil {
+		log.Fatalf("failed to get content rows")
+	}
+
+	// helper function
+	parseTotal := func(v string) int {
+		regex := regexp.MustCompile("\\D")
+		str := regex.ReplaceAllString(v, "")
+		fmt.Println(str)
+		res, _ := strconv.Atoi(str)
+		return res
+	}
+
+	// Write CSV data to a file
+	file, err := os.OpenFile("instagram.csv", os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("could not create file: %v", err)
+	}
+	defer file.Close()
+
+	csvData := "sep=,\nusername,post,date,url,like,comment,is_repost\n"
+	_, err = file.WriteString(csvData)
+	if err != nil {
+		log.Fatalf("could not write to file: %v", err)
+	}
+
+	maxFetchedData := 10
+	fetchedData := 0
+	results := []Result{}
+	// TODO handling infinite scrolling
+	// https://stackoverflow.com/questions/69183922/playwright-auto-scroll-to-bottom-of-infinite-scroll-pag
+	for _, row := range rows {
+		if fetchedData > maxFetchedData {
+			break
+		}
+		if columns, err := row.Locator("> div").All(); err != nil {
+			log.Fatalf("failed to get columns row")
+		} else {
+			for _, column := range columns {
+				fetchedData++
+				if fetchedData > maxFetchedData {
+					break
+				}
+				column.Hover()
+				newEntry := Result{
+					Link:         "",
+					TotalComment: 0,
+					TotalLike:    0,
+					Username:     "",
+					Date:         time.Time{},
+					Post:         "",
+				}
+
+				if onHoverContent, err := column.Locator("a").AllInnerTexts(); err != nil {
+					log.Fatalf("failed to get like & comment: %v", err)
+				} else {
+					contentSplit := strings.Split(onHoverContent[0], "\n")
+					commentTotal := parseTotal(contentSplit[0])
+					likeTotal := parseTotal(contentSplit[1])
+					fmt.Printf("comment total: %v, like total: %v \n", commentTotal, likeTotal)
+					newEntry.TotalComment = commentTotal
+					newEntry.TotalLike = likeTotal
+				}
+
+				if link, err := column.Locator("a").GetAttribute("href"); err != nil {
+					log.Fatalf("failed to get link: %v", err)
+				} else {
+					fmt.Printf("post link: %v \n", link)
+					newEntry.Link = fmt.Sprintf("https://instagram.com/%v", link)
+				}
+
+				column.Click()
+				postDescLocator := "article[role=\"presentation\"] > div > div:nth-child(2) > div > div > div:nth-child(2) > div > ul > div:first-child"
+				descLocator := page.Locator(postDescLocator).First()
+
+				selectorUsername := "> li > div > div > div:nth-child(2) > h2 a"
+				if username, err := descLocator.Locator(selectorUsername).InnerText(); err != nil {
+					log.Fatalf("failed to get username: %v", err)
+				} else {
+					fmt.Printf("username: %v \n", username)
+					newEntry.Username = username
+				}
+
+				descTextLocator := "> li > div > div > div:nth-child(2) > div"
+				if descText, err := descLocator.Locator(descTextLocator).Nth(0).InnerText(); err != nil {
+					log.Fatalf("failed to get description text: %v", err)
+				} else {
+					fmt.Printf("desc: %v \n", descText)
+					newEntry.Post = descText
+				}
+
+				if rawDateTime, err := descLocator.Locator(descTextLocator).Nth(1).Locator("> span time[datetime]").GetAttribute("datetime"); err != nil {
+					log.Fatalf("failed to get datetime: %v", err)
+				} else {
+					dateTime, err := time.Parse("2006-01-02T15:04:05.000Z", rawDateTime)
+					if err != nil {
+						log.Fatalf("failed to parse time: %v", err)
+					}
+					fmt.Printf("desc: %v \n", dateTime)
+					newEntry.Date = dateTime
+				}
+
+				if err := page.Locator("svg[aria-label=\"Close\"]").First().Click(); err != nil {
+					log.Fatalf("failed to close post modal: %v", err)
+				}
+				if err := page.WaitForURL(targetURI); err != nil {
+					log.Fatalf("browser is not redirected back to profile page: %v", err)
+				}
+				results = append(results, newEntry)
+
+				csvData = newEntry.ToCSV()
+				_, err = file.WriteString(csvData)
+				if err != nil {
+					log.Fatalf("could not write to file: %v", err)
+				} else {
+					fmt.Printf("post dumped to Csv: %v", fetchedData)
+				}
+			}
+		}
+	}
 }
